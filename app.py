26a7b37954486616f927672ae96962ded00a2fe0
David  Beamish <davidrbeamish@gmail.com>
2020-07-13 16:34:01 +0000

    added creat function for review is not working as presently displayed

--- app.py
+++ app.py
@@ -13,8 +13,21 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/get_games")
 def get_games():
-    return render_template("index.html", games=mongo.db.games.find(), consoles=mongo.db.consoles.find())
+    return render_template("index.html", 
+                           games=mongo.db.games.find(), consoles=mongo.db.consoles.find())
 
+
+@app.route("/add_review")
+def add_review():
+    return render_template("addreview.html",  consoles=mongo.db.consoles.find(),
+                           genres=mongo.db.genre.find(), scores=mongo.db.score.find())
+
+
+@app.route('/insert_game', methods=['POST'])
+def insert_game():
+    games = mongo.db.games
+    games.insert_one(request.form.to_dict())
+    return redirect(url_for("get_games"))
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
