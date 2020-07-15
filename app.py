fe8913f07faec45f0b14c800f7a7f90f048f9600
David  Beamish <davidrbeamish@gmail.com>
2020-07-15 15:07:45 +0000

    made a new page for adding games but have failed to connect it so far, messed around with game page

--- app.py
+++ app.py
@@ -22,26 +22,33 @@ def list_games():
     return render_template("games-list.html", games=mongo.db.games.find())
 
 
-@app.route("/game_page")
-def game_page():
-    return render_template("games-page.html", games=mongo.db.games.find(),
-                           images=mongo.db.games.image.find())
+@app.route("/game_page/<int:game_id>", methods=["GET"])
+def game_page(game_id):
+    the_game =  mongo.db.games.find_one({"_id": game_id})
+    return render_template("game-page.html", games=mongo.db.games.find(), this_game=the_game)
 
 
 @app.route("/add_game")
 def add_game():
-    return render_template("addreview.html",  consoles=mongo.db.consoles.find(),
+    return render_template("addgame.html", consoles=mongo.db.consoles.find(),
+                           games=mongo.db.games.find())
+
+
+@app.route("/insert_game", methods=['POST'])
+def insert_game():
+    col.insert_one
+    return redirect(url_for('list_games'), consoles=mongo.db.consoles.find(),
+                           genres=mongo.db.genre.find(), scores=mongo.db.score.find(),
+                           games=mongo.db.games.find())
+    
+
+@app.route("/add_review")
+def add_review():
+    return render_template("addreview.html", consoles=mongo.db.consoles.find(),
                            genres=mongo.db.genre.find(), scores=mongo.db.score.find(),
                            games=mongo.db.games.find())
 
 
-@app.route("/insert_game", methods=["POST"])
-def insert_game():
-    games = mongo.db.games
-    games.insert_one(
-        {}
-    )
-    return redirect(url_for("get_games"))
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
