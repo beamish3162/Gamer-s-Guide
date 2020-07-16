3a4118a40ec7c91d0b5b6267655d575d7cd647d8
David  Beamish <davidrbeamish@gmail.com>
2020-07-16 12:11:34 +0000

    added full add game functionality and fixed up the loops for reviews

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/get_games")
 def get_games():
-    return render_template("index.html", 
+    return render_template("index.html",
                            games=mongo.db.games.find(), consoles=mongo.db.consoles.find())
 
 
@@ -22,25 +22,46 @@ def list_games():
     return render_template("games-list.html", games=mongo.db.games.find())
 
 
-@app.route("/game_page/<int:game_id>", methods=["GET"])
+@app.route("/game_page/<game_id>", methods=["GET"])
 def game_page(game_id):
-    the_game =  mongo.db.games.find_one({"_id": game_id})
-    return render_template("game-page.html", games=mongo.db.games.find(), this_game=the_game)
+    the_game = mongo.db.games.find_one({"_id": ObjectId(game_id)})
+    return render_template("game-page.html", game=the_game)
 
 
 @app.route("/add_game")
 def add_game():
     return render_template("addgame.html", consoles=mongo.db.consoles.find(),
-                           games=mongo.db.games.find())
-
-
-@app.route("/insert_game", methods=['POST'])
-def insert_game():
-    col.insert_one
-    return redirect(url_for('list_games'), consoles=mongo.db.consoles.find(),
                            genres=mongo.db.genre.find(), scores=mongo.db.score.find(),
                            games=mongo.db.games.find())
-    
+
+
+@app.route("/insert_game", methods=["POST"])
+def insert_game():
+    games = mongo.db.games
+
+    name = request.form['name']
+    console_type = request.form['console_type']
+    genre_type = request.form['genre_type']
+    image = request.form['image']
+    reviewer = request.form['reviewer']
+    rating = request.form['rating']
+    comments = request.form['comments']
+
+    game_form = {
+        'name': name,
+        'console_type': console_type,
+        'genre_type': genre_type,
+        'image': image,
+        'review':[ {
+            'reviewer': reviewer,
+            'rating': rating,
+            'comments': comments
+        }]
+
+    }
+    games.insert_one(game_form)
+    return redirect(url_for("list_games"))
+
 
 @app.route("/add_review")
 def add_review():
